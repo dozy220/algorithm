baf91583b44f2ff893c4068c91efce66c92bc162
dozy <133772097+dozy316@users.noreply.github.com>
2023-08-03 09:06:28 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        int[] ant = new int[] {5, 3, 1};
+        
+        for (int i=0; i<ant.length; i++) {
+            answer += hp/ant[i];
+            hp = hp%ant[i];
+        }
+        
+        return answer;
+    }
+}
