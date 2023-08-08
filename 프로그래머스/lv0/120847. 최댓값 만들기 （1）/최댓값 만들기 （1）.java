8f0b3d2cefddf4b79ee52f17405cae815f33629f
dozy <133772097+dozy316@users.noreply.github.com>
2023-08-08 11:17:09 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.37 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -3,8 +3,11 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] numbers) {
 
-        int[] ans = Arrays.stream(numbers).sorted().toArray();
+        Arrays.sort(numbers);
         
-        return ans[ans.length-2] * ans[ans.length-1];
+        return numbers[numbers.length-2] * numbers[numbers.length-1];
+//         int[] ans = Arrays.stream(numbers).sorted().toArray();
+        
+//         return ans[ans.length-2] * ans[ans.length-1];
     }
 }
