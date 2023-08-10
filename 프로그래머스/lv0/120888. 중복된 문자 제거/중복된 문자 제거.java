93f34b5262bff2892582e464d4dceec7ce3b943c
dozy <133772097+dozy316@users.noreply.github.com>
2023-08-10 10:42:29 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.41 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -3,11 +3,9 @@ import java.util.LinkedHashSet;
 class Solution {
     public String solution(String my_string) {
         LinkedHashSet<String> str = new LinkedHashSet<>();
-        StringBuffer answer = new StringBuffer();
-        
+                
         for (String s : my_string.split("")) str.add(s);
-        for (String s : str) answer.append(s);
         
-        return answer.toString();
+        return String.join("", str);
     }
 }
