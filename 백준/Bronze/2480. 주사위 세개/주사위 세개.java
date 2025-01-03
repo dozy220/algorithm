806cb4f7450ebb3ab57ad5b7f20f15916c79eb84
dozy <133772097+dozy220@users.noreply.github.com>
2025-01-04 00:06:02 +0900

    [Bronze IV] Title: 주사위 세개, Time: 220 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        sc.close();
+
+        int max = 0;
+        if (a < b) {
+            if (b < c) max = c;
+            else max = b;
+        } else {
+            if (a < c) max = c;
+            else max = a;
+        }
+
+        if (a == b && b == c) {
+            System.out.println(10000+(a*1000));
+        } else if (a == b || b == c || a == c) {
+            if (a == b) System.out.println(1000+(a*100));
+            else if (b == c) System.out.println(1000+(b*100));
+            else System.out.println(1000+(c*100));
+        } else {
+            System.out.println((max*100));
+        }
+    }
+}
