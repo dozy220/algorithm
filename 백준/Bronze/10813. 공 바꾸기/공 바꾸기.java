a56706ec5c757953498828f86fbcccaa9853eb5a
dozy <133772097+dozy220@users.noreply.github.com>
2025-01-06 20:44:37 +0900

    [Bronze II] Title: 공 바꾸기, Time: 228 ms, Memory: 17920 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int n = sc.nextInt();
+        int[] basket = new int[size];
+
+        for (int i=1; i<=size; i++) {
+            basket[i-1] = i;
+        }
+
+        for (int i=0; i<n; i++) {
+            int a = sc.nextInt()-1;
+            int b = sc.nextInt()-1;
+            int tmp = 0;
+
+            tmp = basket[a];
+            basket[a] = basket[b];
+            basket[b] = tmp;
+        }
+        sc.close();
+        System.out.println(Arrays.toString(basket)
+                .replace("[", "")
+                .replace("]", "")
+                .replace(",", ""));
+    }
+}
