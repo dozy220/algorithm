765a2a1e178e358df430b11796106914fb2a018c
dozy <133772097+dozy220@users.noreply.github.com>
2025-01-06 22:39:48 +0900

    [Bronze II] Title: 단어의 개수, Time: 448 ms, Memory: 30692 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,16 +1,11 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
 
-        int cnt = 0;
-        while(sc.hasNext()) {
-            cnt++;
-            sc.next();
-        }
+        StringTokenizer st = new StringTokenizer(sc.nextLine(), " ");
         sc.close();
-
-        System.out.println(cnt);
+        System.out.println(st.countTokens());
     }
 }
