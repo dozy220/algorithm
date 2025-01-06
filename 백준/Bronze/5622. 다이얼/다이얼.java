27a8f90b37b24fc27979b3f63872d12b27dd9a4f
dozy <133772097+dozy220@users.noreply.github.com>
2025-01-06 23:42:18 +0900

    [Bronze II] Title: 다이얼, Time: 168 ms, Memory: 17560 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -5,17 +5,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         sc.close();
+        
         char[] exep = {'S', 'V', 'Y', 'Z'};
-
         int sec = 0;
 
         for (int i=0; i<str.length(); i++) {
             for (char ch : exep)
                 if (str.charAt(i) == ch) sec -= 1;
-            sec += (str.charAt(i) - 62) / 3;
+            sec += (str.charAt(i) - 'A' + 9) / 3;
         }
-
-        sec += str.length()*2;
+        
         System.out.println(sec);
     }
 }
