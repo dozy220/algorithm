8fbd03411132ef97c8c447f4cad9696f7beb6ef6
dozy <133772097+dozy220@users.noreply.github.com>
2025-01-07 22:41:02 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 192 ms, Memory: 18332 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -4,13 +4,14 @@ public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int[] chess = {1, 1, 2, 2, 2, 8};
-        int[] needs_chess = new int[chess.length];
 
         for (int i=0; i<chess.length; i++) {
-            needs_chess[i] = chess[i] - sc.nextInt();
+            chess[i] -= sc.nextInt();
         }
         sc.close();
 
-        for (int i=0; i<needs_chess.length; i++) System.out.print(needs_chess[i]+" ");
+        for (int i : chess) {
+            System.out.print(i + " ");
+        }
     }
 }
