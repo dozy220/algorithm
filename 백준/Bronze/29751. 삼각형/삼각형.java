c0f44dba91c9593d85f7989c6de26b6c43ef304d
dozy <133772097+dozy220@users.noreply.github.com>
2025-01-09 20:39:32 +0900

    [Bronze V] Title: 삼각형, Time: 172 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+
+        System.out.println((w*h)/2.0);
+    }
+}
+
